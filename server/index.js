2db632733b87a4be1baa3d8df335bce9598bee64
William <wabarringer@gmail.com>
2023-03-13 10:33:06 -0700

    delete client practice

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ const io = new Server(server, {
 //   },
 // });
 
+let rooms = {};
 // 1) Listen to user connection
 io.on("connection", (socket) => {
   console.log("New user connected");
@@ -28,18 +29,38 @@ io.on("connection", (socket) => {
   // 2) Listen for host
   socket.on("host", (roomId) => {
     console.log(`User hosted room ${roomId}`);
-
     // 2) Create new room
     const room = io.of(`/${roomId}`);
   });
 
   // 3) Listen for join
+  // Join event here is run when the user navigates to that page.
   socket.on("join", ({ roomId, username }) => {
     // Join the user
     socket.join(roomId);
+    // Create an array of users with the KEY of the room id. Ex; { 14: { users: [] } } if it does not exist
+    console.log(`user joined ${roomId} username ${username}`);
+    if (!rooms[roomId]) {
+      rooms[roomId] = { users: [] };
+    }
 
-    // 3) Broadcast to room
-    socket.to(roomId).emit("user-joined", { username });
+    // Push current users to array of users for that room key.
+    rooms[roomId].users.push(username);
+    console.log(`Current users joined ${roomId} are ${rooms[roomId].users}`);
+
+    // emit to users in room
+    socket.to(roomId).emit("user-joined", username);
+  });
+
+  // ALL EVENTS IN HERE ARE RUN INSIDE OF THE ROOM
+  socket.on("in-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user array", rooms[roomId].users);
+
+    socket.on("send-message", ({ username, message }) => {
+      let newMsg = `${username}: ${message}`;
+      socket.to(roomId).emit("return-message", newMsg);
+    });
   });
 });
 
